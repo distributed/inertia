e4541f8a4bd2065d992e191e2968675ad8d61ca3
Michael Meier <distributed@gloenggi.ch>
2012-02-15 23:36:08 +0100

    scroll.c: added some comments and TODOs

--- scroll.c
+++ scroll.c
@@ -12,7 +12,7 @@ void scroll_init(volatile scrollstate *s, uint8_t *string, uint8_t *font, uint8_
 	s->bytecnt = 0;
 	s->increment = 1;
     } else {
-	s->bitcnt = 7;
+	s->bitcnt = 7;  // TODO: SCROLL_FONTWIDTH(s)-1 ?
 	s->bytecnt = s->len-1;
 	s->increment = (uint8_t) -1;
     }
@@ -28,11 +28,15 @@ uint16_t scroll_step(volatile scrollstate *s) {
     uint8_t ch = pgm_read_byte(&(s->string[s->bytecnt]));
 
     //uint16_t boffset = SCROLL_FONTWIDTH(s)*ch;
-    // for fontwidth == 5:
+    // for fontwidth == 5:  // TODO: find smart way to encode mult without mult
+    // for the general case
     uint16_t boffset = (ch << 2) + ch;
     uint8_t col = pgm_read_byte(s->font + (boffset) + s->bitcnt);
 
     s->bitcnt += s->increment;
+    // TODO: >= even for unsigned type and increment -1?
+    // it works because 0-1 == 255, but it's a bit ugly and at
+    // least worth a comment
     if (s->bitcnt >= SCROLL_FONTWIDTH(s)) {
 
 	if (s->increment == 1) {
@@ -44,7 +48,7 @@ uint16_t scroll_step(volatile scrollstate *s) {
 	s->bytecnt += s->increment;
 	if (s->bytecnt >= s->len) {
 	    s->active = 0;
-	    return col;
+	    return col; // terminates early in case of all-off column
 	}
     }
     return (1<<8) | col;
