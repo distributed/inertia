e0d3fb2317b537a0b8e98d6c60675c0d68102b25
Michael Meier <flowmaster@gloenggi.ch>
2012-02-20 17:27:26 +0100

    main.c: more comments

--- main.c
+++ main.c
@@ -11,6 +11,77 @@
 
 #define SLEEP_INHIBIT (20)
 
+/* 
+  Intended Target: An ATiny44 microcntroller equipped with
+  a column of 8 LEDs for use as a POV device. A 
+  button/reed switch controls when a new text is to be drawn.
+  
+
+  Operation: At the push of a button connected to PB2 (INT0), the microcontroller
+  lights up its column of 8 LEDs according to the message selected in the main
+  loop. If a message is being flashed out, but there's no work to do in the
+  main loop, the controller enters a shallow sleep mode. If the main loop's
+  out of work and the controller is not currently flashing a message, the
+  controller goes into a deep sleep mode.
+  
+  
+  Hardware connections:
+  PA0		-		LED0 (anode, cathode to ground)
+  PA1		-		LED1 (anode, cathode to ground)
+  PA2		-		LED2 (anode, cathode to ground)
+  PA3		-		LED3 (anode, cathode to ground)
+  PB0		-		LED4 (anode, cathode to ground)
+  PB1		-		LED5 (anode, cathode to ground)
+  PA5		-		LED6 (anode, cathode to ground)
+  PA7		-		LED7 (anode)
+  PA6		-		LED7 (cathode)
+  PB2		-		Push button/reed switch, internally pulled-up
+  
+  LED0 is at the top, LED7 is at the bottom. All the
+  standard ISP pins should be connected.
+  
+  
+  While the controller is not in deep sleep mode, the system clock
+  is running and feeding Timer 0. Timer 0 is set up to generate an
+  Output Compare Match A interrupt every so often. (2.5 ms when the
+  device is supposed to be moved manually) The interrupt routine
+  calls scroll_step to deduce which LEDs to light up in the current
+  position in the text. A return value of 0 means that the LEDs
+  should be off. The LEDs are set using output_column which
+  demangles logical LED numbers to the actual pin locations.
+  The scroll state machine turns itself off automatically at
+  the end of the message.
+  After the scroll logic, the ISR debounces the input keys. Every way through
+  the ISR, key_update is set to 1 to indicate new valid key state.
+  The ISR also sets isr_attention to 1 in order to signal to the
+  main loop that it needs to be run completely at least one more
+  time. isr_attention != 0 prevents the controller from going to
+  sleep, preventing sleep when there's pending data from interrupts.
+  
+  The main loop checks for the key being pressed. If an edge
+  leading from unpressed -> depressed is detected, the main
+  loop sets up the scroll state with a new message to display.
+  This is done with a call to scroll_step(). The scroll state
+  machine is then activated by setting .active to 1. The controller
+  starts displaying the message the next time the TIMER0_COMPA 
+  is called. Messages are selected consecutively out of the
+  constant buffer msgs. It contains a number of '\0' terminated
+  strings, its end is marked by a double '\0'.
+  
+  At the end of the main loop, the shallow and deep sleep modes
+  are manages. The controller may enter shallow sleep mode
+  during the display of a message or some time (set by SLEEP_INHIBIT)
+  after. If the main loop has been run completely one time
+  with isr_attention == 0, no stimuli are present and the controller
+  may go to deep sleep. The controller can only be woken up from
+  deep by generating an INT0 interrupt. The INT0 interrupt routine
+  is almost a stub - it only clears its own interrupt enable to
+  prevent the controller from being overwhelmed by a large
+  number of interrupts generated to key bounce.
+
+ */
+
+
 uint8_t isr_attention;
 
 scrollstate scroll;
@@ -45,7 +116,7 @@ void ioinit() {
 }
 
 
-
+// debounce routines by Peter Danegger
 volatile uint8_t key_state;   // debounced and inverted key state:
                               // bit = 1: key pressed
 volatile uint8_t key_press;   // key press detect
